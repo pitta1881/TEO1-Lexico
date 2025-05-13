f2cfde762c047b85642ad05212dbbd9d14f64c3d
Patricio Pittavino <pitta1881@gmail.com>
2025-05-13 00:08:24 -0300

    add child nodes to Write node

--- src/main/java/com/Lexico/FlexLexico/nodes/NodeWrite.java
+++ src/main/java/com/Lexico/FlexLexico/nodes/NodeWrite.java
@@ -1,9 +1,24 @@
 package com.Lexico.FlexLexico.nodes;
 
+import java.util.List;
+
 public class NodeWrite extends Node {
 
-    public NodeWrite() {
+    private List<Node> listNodeText;
+
+    public NodeWrite(List<Node> listNodeText) {
         super("WRITE");
+        this.listNodeText = listNodeText;
+    }
+
+    @Override
+    protected String graph(String idPadre) {
+        final String myId = this.getIdNode();
+        return super.graph(idPadre) + 
+            listNodeText.stream()
+                .sorted((a, b) -> -1) // Reverse the list
+                .map(node -> node.graph(myId))
+                .reduce("", String::concat);
     }
 
 }
